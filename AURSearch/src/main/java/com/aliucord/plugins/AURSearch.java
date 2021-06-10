a1d2d05597d3fc9942e9d5b78ad69a7e84299e75
Alyxia Sother <lexisoth2005@gmail.com>
2021-06-10 13:12:57 +0200

    [AURSearch] Added plugin

--- AURSearch/src/main/java/com/aliucord/plugins/AURSearch.java
+++ AURSearch/src/main/java/com/aliucord/plugins/AURSearch.java
@@ -13,9 +13,12 @@ package com.aliucord.plugins;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+
 import com.aliucord.Http;
 import com.aliucord.api.CommandsAPI;
+import com.aliucord.entities.MessageEmbedBuilder;
 import com.aliucord.entities.Plugin;
+import com.aliucord.plugins.aursearch.ApiResponse;
 import com.discord.api.commands.ApplicationCommandType;
 import com.discord.api.message.embed.MessageEmbed;
 import com.discord.models.commands.ApplicationCommandOption;
@@ -23,15 +26,16 @@ import com.discord.models.commands.ApplicationCommandOption;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @SuppressWarnings("unused")
-public class    DiscordJS extends Plugin {
+public class AURSearch extends Plugin {
     @NonNull
     @Override
     public Manifest getManifest() {
         var manifest = new Manifest();
         manifest.authors = new Manifest.Author[] { new Manifest.Author("Alyxia", 465702500146610176L) };
-        manifest.description = "DiscordJS Documentation searcher.";
+        manifest.description = "Search the AUR.";
         manifest.version = "1.0.0";
         manifest.updateUrl = "https://raw.githubusercontent.com/lexisother/AliucordPlugins/builds/updater.json";
         return manifest;
@@ -43,23 +47,45 @@ public class    DiscordJS extends Plugin {
         arguments.add(new ApplicationCommandOption(ApplicationCommandType.STRING, "query", "What to search for.", null, true, true, null, null));
 
         commands.registerCommand(
-                "docs",
-                "Searches the Discord.JS docs.",
+                "aur",
+                "Searches the AUR.",
                 arguments,
                 args -> {
                     String query = (String) args.get("query");
 
                     if (query == null) return new CommandsAPI.CommandResult("You did not specify a query.", null, false);
 
-                    MessageEmbed res = null;
+                    ApiResponse res = null;
+                    MessageEmbedBuilder embed = null;
+                    MessageEmbed finishedEmbed = null;
+                    Integer i = 0;
                     String result;
                     try {
-                        res = Http.simpleJsonGet("https://djsdocs.sorta.moe/v2/embed?src=master&q=" + query, MessageEmbed.class);
+                        String baseURL = "https://aur.archlinux.org/rpc/?v=5&type=search&arg=";
+                        String aurURL = "https://aur.archlinux.org/packages/";
+                        res = Http.simpleJsonGet(baseURL + query, ApiResponse.class);
                         result = "I found the following:";
+
+                        embed = new MessageEmbedBuilder()
+                                .setColor(2266867)
+                                .setTitle("AUR Search")
+                                .setDescription("For all results, click [here](" + aurURL + "?K=" + query + ")")
+                                .setFooter(res.resultcount + " results found, showing first 5.", null);
+
+                        for (ApiResponse.Package pkg : res.results) {
+                            if (i < 5) {
+                                embed.addField(pkg.Name, pkg.Description + "\n" + String.format("[link](%s)", aurURL + pkg.Name), false);
+                                i++;
+                            } else {
+                                break;
+                            }
+                        }
+
+                        finishedEmbed = embed.build();
                     } catch (IOException t) {
                         result = "Something went wrong: " + t.getMessage();
                     }
-                    return new CommandsAPI.CommandResult(result, Collections.singletonList(res), false);
+                    return new CommandsAPI.CommandResult(result, Collections.singletonList(finishedEmbed), false);
                 }
         );
     }
